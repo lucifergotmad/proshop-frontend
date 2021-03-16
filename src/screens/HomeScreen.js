692b1e442f16e78ac2b4e83a8c02126f52916d89
L u c i f e r <amateur.fullstackdev@gmail.com>
2021-03-16 11:03:25 +0700

    adding axios and fetching data from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      const { data } = response;
+      setProducts(data);
+    });
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
