8ee4af05b1152198ab6f3ce6e291f39f83ec8086
L u c i f e r <amateur.fullstackdev@gmail.com>
2021-03-20 22:58:09 +0700

    adding LoginScreen, UserLoginReducer and many more

--- src/store.js
+++ src/store.js
@@ -4,23 +4,31 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import {
   productDetailsReducer,
   productListReducer,
-} from "./reducers/productReducers.js";
-
-import { cartReducer } from "./reducers/cartReducers.js";
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: {
     cartItems: cartItemFromStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage,
   },
 };
 
